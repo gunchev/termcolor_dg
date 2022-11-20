6c717e129c4a48f0467788a4e55c2bb9cb008372
Doncho N. Gunchev <dgunchev@gmail.com>
2022-11-21 01:09:45 +0200

    Version 0.9.3.1

--- src/termcolor_dg.py
+++ src/termcolor_dg.py
@@ -42,7 +42,7 @@ import time
 __all__ = ['always_colored', 'colored', 'cprint', 'rainbow_color', 'monkey_patch_logging', 'logging_basic_color_config',
            'COLOR_RESET_STR']
 
-__version__ = '0.9.3'
+__version__ = '0.9.3.1'
 __copyright__ = 'Copyright (c) 2008-2011 Volvox Development Team'
 __license__ = 'MIT'
 
